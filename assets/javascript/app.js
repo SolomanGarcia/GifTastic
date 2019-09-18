8362f92fb254e9065ba9d3c463d939dfb727273a
Soloman Garcia <soloman.garcia@gmail.com>
2019-09-18 17:37:46 -0600

    debugged, JS now fully functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,61 +10,74 @@ function displayCelebrityGifs() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        console.log(response)
+    }).then(function (response) {
+        console.log(response);
 
         var results = response.data;
-        
+
         for (var i = 0; i < results.length; i++) {
 
             //display rating
             var p = $("<p>");
             p.text(results[i].rating);
             var p = $("<p>").text("Rating: " + results[i].rating);
-        
-    //creating a new div to hold the Celebrity
-    var celebrityDiv = $("<div class='celebrity'>");
 
-//    var gifURL = response.data.url;
-
-//    console.log(results);
-
-   var celebrityGif = $("<img>")
-//    .attr("src", gifURL);
-
-   celebrityGif.attr("src", results[i].images.fixed_height_still.url);
-//    celebrityGif.attr("data-still", results[i].fixed_height_still.url);
-//    celebrityGif.attr("data-animate", results[i].fixed_height.url);
-//    celebrityGif.attr("data-state", "still");
-//    celebrityGif.addClass("gif");
+            //creating a new div to hold the Celebrity
+            var celebrityDiv = $("<div class='celebrity'>");
 
 
-   celebrityDiv.append(celebrityGif);
 
-   celebrityDiv.append(p);
+            var celebrityGif = $("<img>")
 
-    $("#celebrity-view").prepend(celebrityDiv);
+
+            celebrityGif.attr("src", results[i].images.fixed_height_still.url);
+            celebrityGif.attr("data-still", results[i].images.fixed_height_still.url);
+            celebrityGif.attr("data-animate", results[i].images.fixed_height.url);
+            celebrityGif.attr("data-state", "still");
+            celebrityGif.addClass("gif");
+
+
+            celebrityDiv.append(celebrityGif);
+
+            celebrityDiv.append(p);
+
+            $("#celebrity-view").prepend(celebrityDiv);
         }
     });
-} 
+}
+
+$("#celebrity-view").on("click", ".gif", function (event) {
+    event.preventDefault();
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
 
 //function for displaying celebrity gifs
 function renderButtons() {
 
-//clearing out celebrities so initial celebs dont repeat
-$("#buttons-view").empty();
-//looping through the array of movies
-for (var i = 0; i < celebrities.length; i++) {
-    var a = $("<button>");
-    a.addClass("celebrity-btn");
-    a.attr("data-name", celebrities[i]);
-    a.text(celebrities[i]);
-    $("#buttons-view").append(a);
-    
-}
+    //clearing out celebrities so initial celebs dont repeat
+    $("#buttons-view").empty();
+    //looping through the array of movies
+    for (var i = 0; i < celebrities.length; i++) {
+        var a = $("<button>");
+        a.addClass("celebrity-btn");
+        a.attr("data-name", celebrities[i]);
+        a.text(celebrities[i]);
+        $("#buttons-view").append(a);
+
+    }
 }
 
-$("#add-celebrity").on("click", function(event) {
+$("#add-celebrity").on("click", function (event) {
     event.preventDefault();
 
     var celebrity = $("#celebrity-input").val().trim();
